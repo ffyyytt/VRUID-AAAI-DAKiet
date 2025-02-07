f038c455abb21010e422de456769234798ab3ce5
Dương Anh Kiệt <43516744+ffyyytt@users.noreply.github.com>
2025-02-07 03:20:06 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -51,6 +51,102 @@ elif args.category == "form_body":
 
 test_data = pickle.load(open(f"{args.datapath}/val_data.pkl", "rb")) | pickle.load(open(f"{args.datapath}/test_data.pkl", "rb"))
 
+# class TestDataset(torch.utils.data.Dataset):
+#     def __init__(self, data, img_size=224):
+#         self.data = data
+#         self.img_size = img_size
+#         self.filename = list(data.keys())
+#         self.augmentation = A.Compose([A.LongestMaxSize(max_size=self.img_size, interpolation=1),
+#                                        A.PadIfNeeded(min_height=self.img_size, min_width=self.img_size, border_mode=0, value=(0,0,0))
+#                                       ])
+#         self.transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+#                                                          torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        
+#     def __len__(self):
+#         r = 0
+#         for idx in range(len(self.data.keys())):
+#             r += len([ self.data[self.filename[idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[idx]]["components"])) if self.data[self.filename[idx]]["components"][i]["category"] == args.child_category ])
+            
+#         return r
+
+#     def get_text(self, obj):
+#         text = ""
+#         for key in ["fixed_caption", "discription", "text", "fixed_text"]:
+#             if key in obj:
+#                 if type(obj[key]) is dict:
+#                     text = text + " " + " ".join([v.replace(".", "").replace("\n", "") for v in obj[key].values()])
+#                 else:
+#                     text = text + " " + obj[key].replace(".", "").replace("\n", "")
+#         return text
+
+#     def __getitem__(self, idx):
+#         true_idx = 0
+#         c = len([ self.data[self.filename[true_idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[true_idx]]["components"])) if self.data[self.filename[true_idx]]["components"][i]["category"] == args.child_category ])
+#         while idx >= c:
+#             true_idx += 1
+#             c += len([ self.data[self.filename[true_idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[true_idx]]["components"])) if self.data[self.filename[true_idx]]["components"][i]["category"] == args.child_category ])
+
+#         child_ids = [ [ self.data[self.filename[true_idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[true_idx]]["components"])) if self.data[self.filename[true_idx]]["components"][i]["category"] == args.child_category ][-c + idx] ]
+#         return self.get(true_idx, child_ids)
+    
+#     def get(self, idx, child_ids):
+#         name = self.filename[idx]
+#         parent_ids = [ self.data[name]["components"][i]["object_id"] for i in range(len(self.data[name]["components"])) if self.data[name]["components"][i]["category"] in args.parent_category ] + [-1]
+#         child_texts = [""]*len(child_ids)
+#         parent_texts = [""]*len(parent_ids)
+#         child_images = [np.zeros([3, self.img_size, self.img_size])]*len(child_ids)
+#         parent_images = [np.zeros([3, self.img_size, self.img_size])]*len(parent_ids)
+#         child_metadata = [[-1, -1, -1] for _ in range(len(child_ids))]
+#         parent_metadata = [[-1, -1, -1] for _ in range(len(parent_ids))]
+
+#         for i in range(len(self.data[name]["components"])):
+#             if self.data[name]["components"][i]["object_id"] in child_ids:
+#                 namer = name.replace("'", "_")
+#                 try:
+#                     page_image = np.asarray(Image.open(f"{args.datapath}//val/val/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+#                 except:
+#                     try:
+#                         page_image = np.asarray(Image.open(f"{args.datapath}/test/test/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+#                     except:
+#                         page_image = np.zeros([2048, 2048, 3])
+#                 object_img = page_image[max(int(self.data[name]["components"][i]['bbox'][1])-50, 0):int(self.data[name]["components"][i]['bbox'][1]+self.data[name]["components"][i]['bbox'][3])+50, 
+#                                         max(int(self.data[name]["components"][i]['bbox'][0])-50, 0):int(self.data[name]["components"][i]['bbox'][0]+self.data[name]["components"][i]['bbox'][2])+50]
+#                 child_texts[child_ids.index(self.data[name]["components"][i]["object_id"])] = self.get_text(self.data[name]["components"][i])
+#                 child_metadata[child_ids.index(self.data[name]["components"][i]["object_id"])] = [self.data[name]["ordered_id"].index(self.data[name]["components"][i]["object_id"]),
+#                                                                                                   self.data[name]["components"][i]["page"],
+#                                                                                                     self.data[name]["components"][i]["category_id"]
+#                                                                                                  ]
+#                 child_images[child_ids.index(self.data[name]["components"][i]["object_id"])] = self.transform(self.augmentation(image=object_img)['image'])
+                
+#             if self.data[name]["components"][i]["object_id"] in parent_ids:
+#                 namer = name.replace("'", "_")
+#                 try:
+#                     page_image = np.asarray(Image.open(f"{args.datapath}/val/val/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+#                 except:
+#                     try:
+#                         page_image = np.asarray(Image.open(f"{args.datapath}/test/test/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+#                     except:
+#                         page_image = np.zeros([2048, 2048, 3])
+#                 object_img = page_image[max(int(self.data[name]["components"][i]['bbox'][1])-50, 0):int(self.data[name]["components"][i]['bbox'][1]+self.data[name]["components"][i]['bbox'][3])+50, 
+#                                         max(int(self.data[name]["components"][i]['bbox'][0])-50, 0):int(self.data[name]["components"][i]['bbox'][0]+self.data[name]["components"][i]['bbox'][2])+50]
+#                 parent_texts[parent_ids.index(self.data[name]["components"][i]["object_id"])] = self.get_text(self.data[name]["components"][i])
+#                 parent_metadata[parent_ids.index(self.data[name]["components"][i]["object_id"])] = [self.data[name]["ordered_id"].index(self.data[name]["components"][i]["object_id"]),
+#                                                                                                     self.data[name]["components"][i]["page"],
+#                                                                                                     self.data[name]["components"][i]["category_id"]
+#                                                                                                    ]
+#                 parent_images[parent_ids.index(self.data[name]["components"][i]["object_id"])] = self.transform(self.augmentation(image=object_img)['image'])
+        
+#         return {
+#             "child_ids": child_ids,
+#             "parent_ids": parent_ids,
+#             "child_texts": child_texts,
+#             "parent_texts": parent_texts,
+#             "child_images": torch.from_numpy(np.asarray(child_images)).to(torch.float32),
+#             "parent_images": torch.from_numpy(np.asarray(parent_images)).to(torch.float32),
+#             "child_metadata": torch.from_numpy(np.asarray(child_metadata)).to(torch.float32),
+#             "parent_metadata": torch.from_numpy(np.asarray(parent_metadata)).to(torch.float32),
+#         }
+
 class TestDataset(torch.utils.data.Dataset):
     def __init__(self, data, img_size=224):
         self.data = data
@@ -63,11 +159,7 @@ class TestDataset(torch.utils.data.Dataset):
                                                          torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         
     def __len__(self):
-        r = 0
-        for idx in range(len(self.data.keys())):
-            r += len([ self.data[self.filename[idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[idx]]["components"])) if self.data[self.filename[idx]]["components"][i]["category"] == args.child_category ])
-            
-        return r
+        return len(self.data.keys())
 
     def get_text(self, obj):
         text = ""
@@ -80,17 +172,8 @@ class TestDataset(torch.utils.data.Dataset):
         return text
 
     def __getitem__(self, idx):
-        true_idx = 0
-        c = len([ self.data[self.filename[true_idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[true_idx]]["components"])) if self.data[self.filename[true_idx]]["components"][i]["category"] == args.child_category ])
-        while idx >= c:
-            true_idx += 1
-            c += len([ self.data[self.filename[true_idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[true_idx]]["components"])) if self.data[self.filename[true_idx]]["components"][i]["category"] == args.child_category ])
-
-        child_ids = [ [ self.data[self.filename[true_idx]]["components"][i]["object_id"] for i in range(len(self.data[self.filename[true_idx]]["components"])) if self.data[self.filename[true_idx]]["components"][i]["category"] == args.child_category ][-c + idx] ]
-        return self.get(true_idx, child_ids)
-    
-    def get(self, idx, child_ids):
         name = self.filename[idx]
+        child_ids = [ self.data[name]["components"][i]["object_id"] for i in range(len(self.data[name]["components"])) if self.data[name]["components"][i]["category"] == args.child_category ]
         parent_ids = [ self.data[name]["components"][i]["object_id"] for i in range(len(self.data[name]["components"])) if self.data[name]["components"][i]["category"] in args.parent_category ] + [-1]
         child_texts = [""]*len(child_ids)
         parent_texts = [""]*len(parent_ids)
@@ -103,10 +186,10 @@ class TestDataset(torch.utils.data.Dataset):
             if self.data[name]["components"][i]["object_id"] in child_ids:
                 namer = name.replace("'", "_")
                 try:
-                    page_image = np.asarray(Image.open(f"{args.datapath}//val/val/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+                    page_image = np.asarray(Image.open(f"dataset/val/val/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
                 except:
                     try:
-                        page_image = np.asarray(Image.open(f"{args.datapath}/test/test/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+                        page_image = np.asarray(Image.open(f"dataset/test/test/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
                     except:
                         page_image = np.zeros([2048, 2048, 3])
                 object_img = page_image[max(int(self.data[name]["components"][i]['bbox'][1])-50, 0):int(self.data[name]["components"][i]['bbox'][1]+self.data[name]["components"][i]['bbox'][3])+50, 
@@ -121,10 +204,10 @@ class TestDataset(torch.utils.data.Dataset):
             if self.data[name]["components"][i]["object_id"] in parent_ids:
                 namer = name.replace("'", "_")
                 try:
-                    page_image = np.asarray(Image.open(f"{args.datapath}/val/val/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+                    page_image = np.asarray(Image.open(f"dataset/val/val/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
                 except:
                     try:
-                        page_image = np.asarray(Image.open(f"{args.datapath}/test/test/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
+                        page_image = np.asarray(Image.open(f"dataset/test/test/{namer}_page-{self.data[name]['components'][i]['page']}.png"))
                     except:
                         page_image = np.zeros([2048, 2048, 3])
                 object_img = page_image[max(int(self.data[name]["components"][i]['bbox'][1])-50, 0):int(self.data[name]["components"][i]['bbox'][1]+self.data[name]["components"][i]['bbox'][3])+50, 
@@ -135,6 +218,9 @@ class TestDataset(torch.utils.data.Dataset):
                                                                                                     self.data[name]["components"][i]["category_id"]
                                                                                                    ]
                 parent_images[parent_ids.index(self.data[name]["components"][i]["object_id"])] = self.transform(self.augmentation(image=object_img)['image'])
+
+        if len(labels) == 0:
+            return self.__getitem__(random.randrange(len(self.filename)))
         
         return {
             "child_ids": child_ids,
